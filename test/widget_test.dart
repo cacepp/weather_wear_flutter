4df6dc8f6c1f6a43c559151bdac3aa81c714f293
cacepp <Iljushin.anton2002@yandex.ru>
2024-11-17 15:38:05 +0300

    test: Исправил импорты

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:WeatherWear/main.dart';
+import 'package:weather_wear_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
